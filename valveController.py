e51b3e9f45b11d10384c2dedea63334d519a9298
DAMIR <damir.mirkovic@fer.hr>
2016-06-21 11:19:37 +0200

    Support for entire 32 valve festo block added

--- valveController.py
+++ valveController.py
@@ -10,8 +10,7 @@ import zmq
 
 subAddr = 'tcp://*:1555'
 valveAddr = '10.42.0.254'
-commandRegAddr = 40003
-
+commandRegAddr = [40003, 40004, 40005, 40006]
 
 festoValveBlock = ModbusTcpClient(valveAddr)
 zmqContext = zmq.Context(1)
@@ -20,8 +19,10 @@ zmqSub.bind(subAddr)
 zmqSub.setsockopt(zmq.SUBSCRIBE, '')
 
 # initialize valve states
-valveStates = 0;
-festoValveBlock.write_register(commandRegAddr, valveStates)
+valveStates = [0, 0, 0, 0];
+
+for i in range(0, 4):
+	festoValveBlock.write_register(commandRegAddr[i], valveStates[i])
 
 command2state = {'On': 1, 'Off': 0}
 
@@ -32,19 +33,23 @@ while 1:
 	# get referenced valve number
 	valve = int(name[-3:])
 	
+	# get valve address byte and number in byte
+	[byteIdx, valveIdx] = divmod(valve-1, 8)
+	valveIdx = valveIdx + 1
+	
 	# check valve state ("on" or "off")
 	if command2state[command] :
 
 		# set valve bit to "1"
-		valveStates = valveStates | (1 << (valve-1))
+		valveStates[byteIdx] = valveStates[byteIdx] | (1 << (valveIdx-1))
 
 	else:
 
 		# set valve bit to "0"
-		valveStates = valveStates & ~(1 << (valve-1))
+		valveStates[byteIdx] = valveStates[byteIdx] & ~(1 << (valveIdx-1))
 
 	# send demanded valve states to FESTO valve block
-	festoValveBlock.write_register(commandRegAddr, valveStates)
+	festoValveBlock.write_register(commandRegAddr[byteIdx], valveStates[byteIdx])
 
 
 
