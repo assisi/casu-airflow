c351fca3c17db820efd5cc40b3847124a806dea4
Damjan Miklic <damjan.miklic@gmail.com>
2016-12-16 13:02:31 +0100

    Added additional printfs to the airflow_sensor

--- sensor/sensor/airflow_sensor.cpp
+++ sensor/sensor/airflow_sensor.cpp
@@ -19,7 +19,8 @@ int main(int argc, char **argv) {
 	int i2c_bus = 2, sensorAddress = 73, inData = 0;
 
 	i2cPIC.initI2C(i2c_bus, sensorAddress);
-	
+
+    printf("Starting airflow measurement...");
 	while (1)	{
 		i2cPIC.receiveData(inBuff, 2);
 		inData = (inBuff[0]<<8) + inBuff[1];
@@ -28,6 +29,10 @@ int main(int argc, char **argv) {
 			printf("Flow: %0.3f SLPM\n\n", roundf(50.0*(((float) inData / 16384.0)-0.1) / 0.8*10) /10);
 			//printf("Flow: %0.3f SLPM\n", 50.0*(((float) inData / 16384.0)-0.1) / 0.8*10);
 		}
+        else
+        {
+            printf("Corrupt data packet received!")
+        }
 		sleep(3);
 		
 	}
